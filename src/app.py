5c1aa5a2742fa776828bd455e5a374572e9f81e0
nineleven <tim-sania2008@mail.ru>
2021-04-30 21:04:01 +0300

    add exception traceback output

--- src/app.py
+++ src/app.py
@@ -1,11 +1,18 @@
 import sys
+import traceback
 
 from PyQt5.QtWidgets import QApplication
 
 from mainwindow import MainWindow
 
 
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
 if __name__ == '__main__':
+    sys.excepthook = except_hook
+    
     app = QApplication(sys.argv)
     app.setStyle('Windows') # for memes
 
