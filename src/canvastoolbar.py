e85352715d52f2e0b088afc3cb102b61740e5ab3
nineleven <tim-sania2008@mail.ru>
2021-04-30 05:07:19 +0300

    add function parsing

--- src/canvastoolbar.py
+++ src/canvastoolbar.py
@@ -1,10 +1,12 @@
-from PyQt5.QtWidgets import QWidget, QLineEdit, QPushButton, QVBoxLayout
+from PyQt5.QtWidgets import QWidget, QLineEdit, QPushButton, QVBoxLayout, QMessageBox
 from PyQt5.QtGui import QRegExpValidator
 from PyQt5.QtCore import QRegExp
 
 import numpy as np
 
 from pathlib import Path
+
+from sympy.parsing.sympy_parser import parse_expr
 
 from canvas import Canvas
 from utils import get_logger
@@ -22,16 +24,18 @@ class CanvasToolBar(QWidget):
 
         self.canvas = canvas
 
-        self.btn_run = QPushButton('run', parent=self)
+        self.btn_run = QPushButton('run')
         self.btn_run.clicked.connect(self.btn_run_clicked)
         
-        self.led_x0 = QLineEdit(parent=self)
+        self.led_x0 = QLineEdit()
         validator = QRegExpValidator(QRegExp('-?[0-9]+(\.[0-9]*)?'))
         self.led_x0.setValidator(validator)
-        
 
+        self.led_func = QLineEdit()
+        
         layout = QVBoxLayout()
 
+        layout.addWidget(self.led_func)
         layout.addWidget(self.led_x0)
         layout.addWidget(self.btn_run)
 
@@ -45,6 +49,15 @@ class CanvasToolBar(QWidget):
             return
 
         x0 = float(self.led_x0.text())
+
+        try:
+            func = parse_expr(self.led_func.text())
+        except SyntaxError:
+            logger.debug('Syntax error in function definition')
+            
+            QMessageBox.warning(self, 'Error', 'Syntax error in function definition', QMessageBox.Ok)
+            return
+            
 
         history = x0 + np.array([[1, 1], [1, 5], [3, 4], [2, 2], [1, 1]])
 
